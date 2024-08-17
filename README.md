9a8d9f2be7dcfda3c3dabf826ff588de47565a7a
palask <48385919+palask@users.noreply.github.com>
2024-08-17 15:06:42 +0200

    Switch to elysia based backend with native WebSockets

--- README.md
+++ README.md
@@ -5,22 +5,19 @@ LAT automatically synchronizes the text you input on the site with every other d
 It also stores the inputted text so it is not lost when you shutdown the server.
 Whenever the server connection is lost, this is displayed to the user.
 
-LAT is build with python and uses Flask as well as Flask-SocketIO on the backend.
-On the frontend it uses Socket.IO to communicate with the server.
+LAT uses [Elysia](https://elysiajs.com/) with [Bun](https://bun.sh/) on the backend.
+On the frontend it has no dependencies, it just uses native WebSockets to communicate with the server.
+Please note that LAT does not use TLS, so it is easy to deploy locally but do not use it for sensitive data.
 
 ## Installation
 
-1. Make sure a recent version of python is installed.
-2. Install the dependencies:
-```sh
-pip install -r requirements.txt
-```
+1. Make sure [Bun](https://bun.sh/) is installed or use [pnpm](https://pnpm.io/).
 
 ## Usage
 
 1. Start the web server with:
 ```sh
-python app.py
+bun run start
 ```
-2. Visit `DEVICE_IP:5000` in your browser and replace `DEVICE_IP` accordingly.
+2. Visit `SERVER_IP:5000` in your browser and replace `SERVER_IP` accordingly.
 3. When you are done, you can shutdown the server with Ctrl+C.
---
+++ server.ts
@@ -0,0 +1,59 @@
+import { Elysia, t } from "elysia";
+
+// --- Settings ---
+// Path to the file where text will be saved
+const TEXT_FILE_PATH = "text_data.txt"
+// Port
+const SERVER_PORT = 5000
+
+let currentText = await readFromFile(TEXT_FILE_PATH)
+let connectedUsers = 0
+
+interface UpdateMessage {
+  text: string;
+  connected_users: string;
+}
+
+function getUpdateMessage(): UpdateMessage {
+  return { text: currentText, connected_users: `${connectedUsers} connected users.` }
+}
+
+async function readFromFile(filePath: string): Promise<string> {
+  const f = Bun.file(filePath)
+  return f.text()
+}
+
+async function writeToFile(filePath: string, text: string) {
+  await Bun.write(filePath, text)
+}
+
+const app = new Elysia()
+
+  .get("/", () => Bun.file("client.html"))
+
+  .ws("/text_update", {
+    body: t.Object({
+      text: t.String()
+    }),
+    message(ws, { text }) {
+      currentText = text
+      writeToFile(TEXT_FILE_PATH, currentText)
+      ws.send(getUpdateMessage())
+    },
+    open(ws) {
+      connectedUsers++
+      console.log(`User connected. Total connected users: ${connectedUsers}`)
+      ws.send(getUpdateMessage())
+    },
+    close(ws) {
+      connectedUsers--
+      console.log(`User disconnected. Total connected users: ${connectedUsers}`)
+      ws.send(getUpdateMessage())
+    }
+  })
+
+  .listen(SERVER_PORT)
+
+console.log(
+  `🦊 LAT is running at ${app.server?.hostname}:${app.server?.port}`
+)
