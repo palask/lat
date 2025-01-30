016ebf28e65be9e5c960cd060ebf4631cfc15b13
palask <48385919+palask@users.noreply.github.com>
2025-01-30 22:45:20 +0100

    Fix socket identity, add cursor updates, restructure server

--- client/script.ts
+++ client/script.ts
@@ -5,12 +5,19 @@ const checkbox = document.getElementById("edit-toggle")! as HTMLInputElement
 
 let socket: WebSocket | null = null
 let reconnectTimer: Timer | null = null
+let reconnectCounter = 1
 
 function createWebSocket() {
     // If there is an existing socket, close it before creating a new one
     if (socket) {
         socket.onclose = null // Prevent triggering the onclose handler
+        if (reconnectTimer) {
+            clearInterval(reconnectTimer)
+            reconnectTimer = null
+        }
         socket.close()
+    } else {
+        connectionStatus.textContent = "Connecting..."
     }
 
     const socketUrl = new URL("text_update", window.location.href)
@@ -22,7 +29,10 @@ function createWebSocket() {
         let data = JSON.parse(event.data)
         inputbox.value = data.text
         previewbox.innerText = data.text
-        connectionStatus.textContent = data.connected_users
+        const connectedUsersMessage = `${data.connected_users} connected user${data.connected_users === 1 ? "" : "s"}.`;
+        connectionStatus.textContent = connectedUsersMessage
+        console.log(data.cursors)
+        // TODO: Filter own cursor, show other cursors
     }
 
     // Notify user if the connection is lost and try to reconnect
@@ -30,15 +40,17 @@ function createWebSocket() {
         console.log("Socket closed")
         if (!reconnectTimer) {
             connectionStatus.textContent = "Connection lost. The server might be offline."
+            reconnectCounter = Math.min(reconnectCounter + 1, 15)
             reconnectTimer = setInterval(() => {
                 createWebSocket()
-            }, 2000)
+            }, reconnectCounter * 1000)
         }
     }
 
     // Clear the warning message when reconnected
     socket.onopen = (event) => {
         console.log("Socket opened")
+        reconnectCounter = 1
         if (reconnectTimer) {
             clearInterval(reconnectTimer)
             reconnectTimer = null
@@ -50,14 +62,32 @@ function createWebSocket() {
 
 createWebSocket()
 
-// Handle text input and emit to server
-inputbox.addEventListener("input", function () {
+function handleTextInputChange() {
     if (!socket) {
         console.error("No open socket")
-        return;
+        return
     }
-    socket.send(JSON.stringify({ text: inputbox.value }))
-})
+    socket.send(JSON.stringify({ text: inputbox.value, cursor: inputbox.selectionStart }))
+}
+
+function handleCursorChange(event: Event) {
+    const navKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "End", "Home"]
+    const navKeyPressed = event instanceof KeyboardEvent && navKeys.includes(event.key);
+    const mousePressed = event instanceof PointerEvent;
+
+    if (navKeyPressed || mousePressed) {
+        if (!socket) {
+            console.error("No open socket")
+            return
+        }
+        socket.send(JSON.stringify({ text: null, cursor: inputbox.selectionStart }))
+    }
+}
+
+// Handle text input and select and emit to server
+inputbox.addEventListener("input", handleTextInputChange)
+inputbox.addEventListener("click", handleCursorChange)
+inputbox.addEventListener("keyup", handleCursorChange)
 
 function configureInputbox() {
     inputbox.readOnly = !checkbox.checked
@@ -69,6 +99,4 @@ function configureInputbox() {
 configureInputbox()
 
 // Handle edit checkbox toggle
-checkbox.addEventListener("change", function () {
-    configureInputbox()
-})
+checkbox.addEventListener("change", configureInputbox)
--- server.ts
+++ server.ts
@@ -9,40 +9,56 @@ const TEXT_FILE_PATH = "text_data.txt"
 // Port
 const SERVER_PORT = 5000
 
-let currentText = await readFromFile(TEXT_FILE_PATH)
-let connectedUsers = 0
+class FileHandler {
+  constructor(private path: string) { }
+
+  async readFromFile(): Promise<string> {
+    const f = Bun.file(this.path)
+    if (await f.exists()) {
+      return f.text()
+    }
+    return ""
+  }
+
+  async writeToFile(text: string) {
+    await Bun.write(this.path, text)
+  }
+}
 
 interface UpdateMessage {
   text: string;
-  connected_users: string;
+  connected_users: number;
+  cursors: object;
 }
 
-type Socket = ElysiaWS<ServerWebSocket<any>, any, any>
+class MessageHandler {
+  private static getUpdateMessage(): UpdateMessage {
+    return { text: state.currentText, connected_users: state.connectedUsers, cursors: Object.fromEntries(state.cursors) }
+  }
 
-function getUpdateMessage(): UpdateMessage {
-  const connectedUsersMessage = `${connectedUsers} connected user${connectedUsers === 1 ? "" : "s"}.`;
-  return { text: currentText, connected_users: connectedUsersMessage }
-}
-
-function sendUpdateMessages(origin: Socket | null = null) {
-  const message = getUpdateMessage()
-  for (const ws of sockets) {
-    if (ws !== origin) {
-      ws.send(message)
+  static sendUpdateMessages(origin: Socket | null = null) {
+    const message = this.getUpdateMessage()
+    for (const ws of state.sockets) {
+      if (!origin || getSocketId(ws) !== getSocketId(origin)) {
+        ws.send(message)
+      }
     }
   }
 }
 
-async function readFromFile(filePath: string): Promise<string> {
-  const f = Bun.file(filePath)
-  if (await f.exists()) {
-    return f.text()
-  }
-  return ""
+type Socket = ElysiaWS<ServerWebSocket<any>, any, any>
+
+const fileHandler = new FileHandler(TEXT_FILE_PATH)
+
+let state = {
+  currentText: await fileHandler.readFromFile(),
+  connectedUsers: 0,
+  cursors: new Map<string, number>(),
+  sockets: [] as Socket[]
 }
 
-async function writeToFile(filePath: string, text: string) {
-  await Bun.write(filePath, text)
+function getSocketId(ws: Socket): string {
+  return ws.raw.data.id
 }
 
 await Bun.build({
@@ -52,32 +68,38 @@ await Bun.build({
   plugins: [html({ inline: true })],
 })
 
-let sockets: Socket[] = []
-
 const app = new Elysia()
 
   .get("/", () => Bun.file("dist/index.html"))
 
   .ws("/text_update", {
     body: t.Object({
-      text: t.String()
+      text: t.Nullable(t.String()),
+      cursor: t.Number()
     }),
-    message(ws, { text }) {
-      currentText = text
-      sendUpdateMessages(ws)
-      writeToFile(TEXT_FILE_PATH, currentText)
+
+    message(ws, { text, cursor }) {
+      if (text !== null) {
+        state.currentText = text
+      }
+      state.cursors.set(getSocketId(ws), cursor)
+      MessageHandler.sendUpdateMessages(ws)
+      fileHandler.writeToFile(state.currentText)
     },
+
     open(ws) {
-      connectedUsers++
-      console.log(`User connected. Total connected users: ${connectedUsers}`)
-      sockets.push(ws)
-      sendUpdateMessages()
+      state.connectedUsers++
+      state.sockets.push(ws)
+      MessageHandler.sendUpdateMessages()
+      console.log(`User connected. Total connected users: ${state.connectedUsers}`)
     },
+
     close(ws) {
-      connectedUsers--
-      console.log(`User disconnected. Total connected users: ${connectedUsers}`)
-      sockets = sockets.filter(e => e !== ws)
-      sendUpdateMessages()
+      state.connectedUsers--
+      state.sockets = state.sockets.filter(e => getSocketId(e) !== getSocketId(ws))
+      state.cursors.delete(getSocketId(ws))
+      MessageHandler.sendUpdateMessages()
+      console.log(`User disconnected. Total connected users: ${state.connectedUsers}`)
     }
   })
 
